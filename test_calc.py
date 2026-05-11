f456334f80a3d2ee0f643c3fb2085f4edd211916
sunilubale123 <136223844+sunilubale123@users.noreply.github.com>
2026-05-11 15:24:32 +0530

    jshudgof

--- test_calc.py
+++ test_calc.py
@@ -4,15 +4,15 @@ from calc import add, sub, mul, div
 def test_add():
     assert add(2, 3) == 5
 
-#def test_sub():
-    assert sub(5, 3) == 2
+def test_sub():
+   assert sub(5, 3) == 2
 
 #def test_mul():
-    assert mul(4, 3) == 12
+    #assert mul(4, 3) == 12
 
 #def test_div():
-    assert div(10, 2) == 5
+   # assert div(10, 2) == 5
 
 #def test_div_by_zero():
-    with pytest.raises(ValueError):
-        div(5, 0)
+    #with pytest.raises(ValueError):
+      #  div(5, 0)
