61955cde6a7c0c58f1642718508613bb3ecca5a1
sunilubale123 <136223844+sunilubale123@users.noreply.github.com>
2026-05-11 15:15:51 +0530

    jshudg

--- test_calc.py
+++ test_calc.py
@@ -7,12 +7,12 @@ def test_add():
 def test_sub():
     assert sub(5, 3) == 2
 
-def test_mul():
+#def test_mul():
     assert mul(4, 3) == 12
 
-def test_div():
+#def test_div():
     assert div(10, 2) == 5
 
-def test_div_by_zero():
+#def test_div_by_zero():
     with pytest.raises(ValueError):
         div(5, 0)
